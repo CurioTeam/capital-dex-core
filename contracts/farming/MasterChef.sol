90cd5b8b2c5889255d1c0864a88289fa0680db8e
Dmitx <37021407+Dmitx@users.noreply.github.com>
2020-11-05 12:05:06 +0300

    Removed unnecessary logic – migration, reward for devs, sushi minting

--- contracts/farming/MasterChef.sol
+++ contracts/farming/MasterChef.sol
@@ -3,24 +3,9 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-// import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-// import "./SushiToken.sol";
 
-
-// interface IMigratorChef {
-//     // Perform LP token migration from legacy UniswapV2 to SushiSwap.
-//     // Take the current LP token address and return the new LP token address.
-//     // Migrator should have full access to the caller's LP token.
-//     // Return the new LP token address.
-//     //
-//     // XXX Migrator must have allowance access to UniswapV2 LP tokens.
-//     // SushiSwap must mint EXACTLY the same amount of SushiSwap LP tokens or
-//     // else something bad will happen. Traditional UniswapV2 does not
-//     // do that so be careful!
-//     function migrate(IERC20 token) external returns (IERC20);
-// }
 
 // MasterChef is the master of Sushi. He can make Sushi and he is a fair guy.
 //
@@ -60,16 +45,12 @@ contract MasterChef is Ownable {
 
     // The SUSHI TOKEN!
     IERC20 public sushi;
-    // // Dev address.
-    // address public devaddr;
     // Block number when bonus SUSHI period ends.
     uint256 public bonusEndBlock;
     // SUSHI tokens created per block.
     uint256 public sushiPerBlock;
     // Bonus muliplier for early sushi makers.
     uint256 public constant BONUS_MULTIPLIER = 10;
-    // // The migrator contract. It has a lot of power. Can only be set through governance (owner).
-    // IMigratorChef public migrator;
 
     // Info of each pool.
     PoolInfo[] public poolInfo;
@@ -86,13 +67,11 @@ contract MasterChef is Ownable {
 
     constructor(
         IERC20 _sushi,
-        // address _devaddr,
         uint256 _sushiPerBlock,
         uint256 _startBlock,
         uint256 _bonusEndBlock
     ) public {
         sushi = _sushi;
-        // devaddr = _devaddr;
         sushiPerBlock = _sushiPerBlock;
         bonusEndBlock = _bonusEndBlock;
         startBlock = _startBlock;
@@ -126,23 +105,6 @@ contract MasterChef is Ownable {
         totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
         poolInfo[_pid].allocPoint = _allocPoint;
     }
-
-    // // Set the migrator contract. Can only be called by the owner.
-    // function setMigrator(IMigratorChef _migrator) public onlyOwner {
-    //     migrator = _migrator;
-    // }
-
-    // // Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
-    // function migrate(uint256 _pid) public {
-    //     require(address(migrator) != address(0), "migrate: no migrator");
-    //     PoolInfo storage pool = poolInfo[_pid];
-    //     IERC20 lpToken = pool.lpToken;
-    //     uint256 bal = lpToken.balanceOf(address(this));
-    //     lpToken.safeApprove(address(migrator), bal);
-    //     IERC20 newLpToken = migrator.migrate(lpToken);
-    //     require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
-    //     pool.lpToken = newLpToken;
-    // }
 
     // Return reward multiplier over the given _from to _to block.
     function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
@@ -192,8 +154,6 @@ contract MasterChef is Ownable {
         }
         uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
         uint256 sushiReward = multiplier.mul(sushiPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-        // sushi.mint(devaddr, sushiReward.div(10));
-        // sushi.mint(address(this), sushiReward);
         pool.accSushiPerShare = pool.accSushiPerShare.add(sushiReward.mul(1e12).div(lpSupply));
         pool.lastRewardBlock = block.number;
     }
@@ -255,10 +215,4 @@ contract MasterChef is Ownable {
             sushi.transfer(_to, _amount);
         }
     }
-
-    // // Update dev address by the previous dev.
-    // function dev(address _devaddr) public {
-    //     require(msg.sender == devaddr, "dev: wut?");
-    //     devaddr = _devaddr;
-    // }
 }
