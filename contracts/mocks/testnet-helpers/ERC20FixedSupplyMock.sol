aaec336e28e48a0597118c058e4aab66182d3d9b
Dmitx <37021407+Dmitx@users.noreply.github.com>
2020-12-11 22:13:22 +0300

    Add ERC20 with fixed supply mock

---
+++ contracts/mocks/testnet-helpers/ERC20FixedSupplyMock.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// mock class using ERC20
+contract ERC20FixedSupplyMock is ERC20 {
+    constructor (
+        string memory name,
+        string memory symbol,
+        address initialAccount,
+        uint256 initialBalance
+    ) public payable ERC20(name, symbol) {
+        _mint(initialAccount, initialBalance);
+    }
+}
