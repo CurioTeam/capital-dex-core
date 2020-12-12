b6fc8da639102d2bfa64a41a9eb5ad49bb2aa179
Dmitx <37021407+Dmitx@users.noreply.github.com>
2020-12-12 20:27:30 +0300

    Change ProxyAdmin after DexWhitelist deployment

--- migrations/3_deploy_whitelist.js
+++ migrations/3_deploy_whitelist.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const { deployProxy } = require("@openzeppelin/truffle-upgrades");
+const { deployProxy, admin } = require("@openzeppelin/truffle-upgrades");
 
 const DexWhitelist = artifacts.require("DexWhitelist");
 
 const owner = "0xB844C65F3E161061bA5D5dD8497B3C04B71c4c83";
 const manager = "0x622153C82dA8E31fB6193c0F8c2768a360f3Db18";
+
+const proxyAdmin = "0xE006A0BB078291e539B3c7b9c8A8aF7f29215600";
 
 module.exports = async function(deployer, network) {
     // get the current deployer address
@@ -32,9 +34,13 @@ module.exports = async function(deployer, network) {
     await dexWhitelist.removeAdmin(curDeployer);
     await dexWhitelist.transferOwnership(owner);
 
+    // transfer ProxyAdmin owner permission
+    await admin.changeProxyAdmin(dexWhitelist.address, proxyAdmin)
+
     // write addresses and ABI to files
     const contractsAddresses = {
-        dexWhitelist: dexWhitelist.address
+        dexWhitelist: dexWhitelist.address,
+        proxyAdmin: proxyAdmin
     };
 
     const contractsAbi = {
