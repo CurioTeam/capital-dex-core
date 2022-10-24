fc15a175fe8d772d4ec4fe79ca948ccc5bd9616c
Vladimir Sushkov <volodya.sushkov@gmail.com>
2022-10-24 18:35:06 +0300

    feat: add Harmony Devnet/Testnet deployed farming contracts

--- migrations/17_add_LPs_to_farming_v2_per_sec.js
+++ migrations/17_add_LPs_to_farming_v2_per_sec.js
@@ -1,7 +1,8 @@
 const {
-    ether
+    ether, BN
 } = require("@openzeppelin/test-helpers");
 
+const IERC20 = artifacts.require("IERC20.sol");
 const MasterChefV2PerSec = artifacts.require("MasterChefV2PerSec.sol");
 
 const masterChefV2PerSecAddress = ""; // TODO: set
@@ -18,8 +19,14 @@ const pools = [{
     }
 ];
 
+const withLPDeposit = false; // TODO: set
+
 module.exports = async function(deployer, network) {
     if (network === "test") return; // skip migrations if use test network
+
+    // get the current deployer address
+    const accounts = await web3.eth.getAccounts();
+    const user = accounts[0];
 
     const masterChefV2 = await MasterChefV2PerSec.at(masterChefV2PerSecAddress);
 
@@ -29,8 +36,33 @@ module.exports = async function(deployer, network) {
         await masterChefV2.add(
           pool.allocPoint,
           pool.pair,
-          false,
+          true,
         );
         console.log("added pair: ", pool.pair);
+    }
+
+    if (withLPDeposit) {
+        console.log("Depositing LP tokens...");
+
+        // stake LP to pools
+        for (const pool of pools) {
+            let pair  = await IERC20.at(pool.pair);
+
+            const amount = (await pair.balanceOf(user)).div(new BN("2"));
+
+            // approve tokens
+            await pair.approve(
+              masterChefV2.address,
+              amount
+            );
+            console.log("approved LP: ", pool.pair);
+
+            // deposit LP
+            await masterChefV2.deposit(
+              pool.pid,
+              amount
+            );
+            console.log("deposited LP: ", pool.pair);
+        }
     }
 };
